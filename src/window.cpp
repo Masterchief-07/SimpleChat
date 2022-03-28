38bf92475361f6319b4139c61dc63371cf1ee8bf
Masterchief-07 <kjonathan1999@hotmail.com>
2022-03-28 12:03:51 +0000

    inprove win

--- src/window.cpp
+++ src/window.cpp
@@ -8,6 +8,7 @@ Window::Window()
 {
 	this->selectionWindow();
 	//this->configServer();
+	//this->configClient();
 }
 
 Window::~Window()
@@ -15,9 +16,9 @@ Window::~Window()
 
 void Window::selectionWindow()
 {
-	auto screen = ScreenInteractive::TerminalOutput();
+	auto screen = ScreenInteractive::Fullscreen();
 	Component server_btn = Button("SERVER",[&]{this->configServer();});
-	Component client_btn = Button("CLIENT",[]{});
+	Component client_btn = Button("CLIENT",[&]{this->configClient();});
 	Component settings_btn = Button("SETTING", []{});
 	Component quit_btn = Button("QUIT", screen.ExitLoopClosure());
 
@@ -30,6 +31,7 @@ void Window::selectionWindow()
 	
 	auto render = Renderer(select_container, [&]{ return
 			vbox({
+				filler(),
 				hbox({ filler(), text("SIMPLE CHAT") | bold , filler()}) | border,
 				hbox({
 					filler(),
@@ -41,6 +43,7 @@ void Window::selectionWindow()
 						}) | border,
 					filler(),
 				}),
+				filler(),
 			});
 		});
 
@@ -52,7 +55,7 @@ void Window::selectionWindow()
 //PRINT THE SERVER IP AND PORT
 void Window::configServer()
 {	
-	auto screen = ScreenInteractive::TerminalOutput();
+	auto screen = ScreenInteractive::Fullscreen();
 	std::string server_ip;
 	std::string server_port;
 
@@ -69,14 +72,61 @@ void Window::configServer()
 			});
 
 	auto render = Renderer(container, [&]{ return
-		hbox({
+		vbox({
 			filler(),
-			vbox({
-				text("SERVER") | bold,
-				hbox({text("IP: "), separator(), ip_input->Render()}),
-				hbox({text("PORT: "), separator(), port_input->Render()}),
-				hbox({create_btn->Render(), cancel_btn->Render()}),
-			}) | border,
+			hbox({
+				filler(),
+				vbox({
+					text("SERVER") | bold,
+					hbox({text("IP: "), separator(), ip_input->Render()}),
+					hbox({text("PORT: "), separator(), port_input->Render()}),
+					hbox({create_btn->Render(), cancel_btn->Render()}),
+				}) | border,
+				filler(),
+			}),
+			filler(),
+		});
+	});
+	
+	screen.Loop(render);
+
+}
+
+void Window::configClient()
+{	
+	auto screen = ScreenInteractive::Fullscreen();
+	std::string username;
+	std::string server_ip;
+	std::string server_port;
+
+	Component username_input = Input(&username, " ");
+	Component ip_input = Input(&server_ip, "127.0.0.1");
+	Component port_input = Input(&server_port, "60000");
+	Component create_btn = Button("CREATE", []{});
+	Component cancel_btn = Button("CANCEL", screen.ExitLoopClosure());
+
+	auto container = Container::Vertical({
+			username_input,
+			ip_input,
+			port_input,
+			Container::Horizontal({
+				create_btn, cancel_btn,}),
+			});
+
+	auto render = Renderer(container, [&]{ return
+		vbox({
+			filler(),
+			hbox({
+				filler(),
+				vbox({
+					text("SERVER") | bold,
+					hbox({text("Username: "), separator(), username_input->Render()}),
+					hbox({text("IP: "), separator(), ip_input->Render()}),
+					hbox({text("PORT: "), separator(), port_input->Render()}),
+					hbox({create_btn->Render(), cancel_btn->Render()}),
+				}) | border,
+				filler(),
+			}),
 			filler(),
 		});
 	});
